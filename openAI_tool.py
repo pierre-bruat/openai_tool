588c4c263c558044a701757544688a78266dc4b8
Pierre Bruat <35839500+pierre-bruat@users.noreply.github.com>
2023-05-02 10:58:49 +0200

    Update openAI_tool.py

--- openAI_tool.py
+++ openAI_tool.py
@@ -238,7 +238,9 @@ if choose =="ContentScoring":
         df['score'], df['missing_terms'] = zip(*df.apply(calculate_score, axis=1))
              # j'affiche le contenu à gauche et le les termes à droite sous forme de tags
         st.metric("Optimization score",df["score"])
-        missing_kw_list = df['missing_terms'].str.split(',').tolist()
+        missing_kw_list = []
+        for row in df['missing_terms']:
+            missing_terms_list.extend(row.split(','))        
         st.write(missing_kw_list)
         missing_kw = st_tags(label = "Missing keywords",text = "Press enter to add more", value= missing_kw_list, suggestions ="", maxtags ="40",key="coucou")
         st.write(missing_kw)
