b2c4a00501a66bfc6176a27dd8e7859ba498e42e
Pierre Bruat <35839500+pierre-bruat@users.noreply.github.com>
2023-05-02 10:02:59 +0200

    Update openAI_tool.py

--- openAI_tool.py
+++ openAI_tool.py
@@ -239,6 +239,7 @@ if choose =="ContentScoring":
              # j'affiche le contenu à gauche et le les termes à droite sous forme de tags
         st.metric("Semantic Score",df["score"])
         missing_kw_list = df.missing_terms.values.tolist()
+        st.write(missing_kw_list)
         missing_kw = st_tags(
             label = "Missing keywords",
             text = "",
