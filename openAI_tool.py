54339587662d2251dc631aab414a7786cf4faf97
Pierre Bruat <35839500+pierre-bruat@users.noreply.github.com>
2023-05-03 09:11:16 +0200

    Update openAI_tool.py

--- openAI_tool.py
+++ openAI_tool.py
@@ -240,6 +240,10 @@ if choose =="ContentScoring":
         st.table(df)
         missing_kw_list = df['missing_terms'].str.split(', ').tolist()
         missing_kw_list = [mot_cle for sous_liste in missing_kw_list for mot_cle in sous_liste]
-        options = st.multiselect('Missing keywords',missing_kw_list)
-        st.write(options)
+        st_tags(value: missing_kw_list,
+            suggestions: ["add new terms"],
+            label: "Enter keywords",
+            text: "Press enter to add more",
+            maxtags: 20,
+            key=1)
 
