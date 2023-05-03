7d09f48a9fd84c160000056a72b9f0bc6f59d73a
Pierre Bruat <35839500+pierre-bruat@users.noreply.github.com>
2023-05-03 16:45:28 +0200

    Update openAI_tool.py

--- openAI_tool.py
+++ openAI_tool.py
@@ -240,5 +240,5 @@ if choose =="ContentScoring":
         st.table(df)
         missing_kw_list = df['missing_terms'].str.split(', ').tolist()
         missing_kw_list = [mot_cle for sous_liste in missing_kw_list for mot_cle in sous_liste]
-        options = st.multiselect('What are your favorite colors', missing_kw_list, default= missing_kw_list)
-        st.write('You selected:', options)
+    options = st.multiselect('What are your favorite colors', missing_kw_list, default= missing_kw_list)
+    st.write('You selected:', options)
