48216012ea2eb58a422ba8d272627f4653abc343
Pierre Bruat <35839500+pierre-bruat@users.noreply.github.com>
2023-05-04 10:30:35 +0200

    Update openAI_tool.py

--- openAI_tool.py
+++ openAI_tool.py
@@ -241,3 +241,6 @@ if choose =="ContentScoring":
         missing_kw_list = df['missing_terms'].str.split(', ').tolist()
         missing_kw_list = [mot_cle for sous_liste in missing_kw_list for mot_cle in sous_liste]
         options = st.multiselect('What are your favorite colors', missing_kw_list, default= missing_kw_list)
+
+    form = st.form(key='my-form-23')
+    options = st.multiselect('What are your favorite colors', missing_kw_list, default= missing_kw_list)
